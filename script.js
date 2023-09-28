5a7e5fa6af1b7cd28d476e19101b0372f23dad9c
NikitaEvchuk <144231215+NikitaEvchuk@users.noreply.github.com>
2023-09-28 15:51:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let cel = 16
+/* let cel = 16
 let far = 9/5 * cel + 32 
 console.log(far)
 
@@ -38,4 +38,51 @@ console.log(Math.sqrt(d))
 let e = 9
 let f = "7"
 console.log(Number(e).toString())
-console.log(Number.parseInt(f))
+console.log(Number.parseInt(f)) */
+
+
+/* let k1 = "5px"
+let k2 = "12djd334"
+let k3 = "12.45asdwe12"
+let k4 =  "qwqweeewq"
+console.log(Number.parseInt(k1))
+console.log(Number.parseInt(k2))
+console.log(Number.parseInt(k3))
+console.log(Number.parseInt(k4)) */
+
+
+
+/* let k1 = "5px"
+let k2 = "12djd334"
+let k3 = "12.45asdwe12"
+let k4 =  "qwqweeewq"
+console.log(Number.parseFloat(k1))
+console.log(Number.parseFloat(k2))
+console.log(Number.parseFloat(k3))
+console.log(Number.parseFloat(k4)) */
+
+
+/* console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18)) */
+
+
+
+/* console.log(Math.random()*(19-3)+3) */
+
+/* let a = 23
+let b = 10
+let S = a*b
+console.log(S + "cm^2") */
+
+/* let h = 10
+let d = 4
+let П = 3.14
+R = d/2
+const V = П*R**2*h
+console.log(V)  */
+
+/* let a = 3 
+let b = 4
+k = Math.pow((a, 2) + b, 2)
+k = Math.sqrt(k)
+console.log(k) */
+
