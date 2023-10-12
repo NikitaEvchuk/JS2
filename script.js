cfafc7218b8d938a03378488f044abe1221e8432
NikitaEvchuk <144231215+NikitaEvchuk@users.noreply.github.com>
2023-10-12 18:53:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,51 @@
-/* let cel = 16
-let far = 9/5 * cel + 32 
-console.log(far)
-
-let month = 30 
-let day = 24 
-let hour = month * day 
-let  minute = hour * 60
-console.log(hour,minute)
-
-
-let healpoint = 100 
-let stamina = 50 
-healpoint = healpoint - 30 
-stamina = stamina - 30 
-console.log( healpoint,stamina)
-
-let price  = 300 
-let sale = price * 10/100
-price = price - sale 
-console.log( price)
-
-let a = 3.07
-console.log(Math.floor(a)) 
-
-
-let b = "2.4jfa"
-console.log(Number.parseFloat(b))
-
-let c = "579atj64"
-console.log(Number.parseInt(c))
-
-let d = 16 
-console.log(Math.sqrt(d))
+/* const isOnline = true;
+const isFriend = true;
+const isDnd = false;
+if(isOnline === true && isFriend === true && isDnd === true){
+    alert("You can")
+}
+else{
+    alert("You cant")
+} */
 
 
 
-let e = 9
-let f = "7"
-console.log(Number(e).toString())
-console.log(Number.parseInt(f)) */
+/* const typeFree = false
+const typePro = true 
+const typeVip = true 
+let person = typeFree
+if(person === typePro && person === typeVip){
+    alert("apply")
+}
+else{
+    alert("Dont have")
+} */
+
+/* const balance = 700 
+const payment = prompt("Ведіть ціну товару")
+alert(`Загальна вартість замовлення ${payment} кредитів. Перевіряємо кількість доступних коштів на рахунку»`)
+if(payment <= balance){
+    a = balance - payment 
+    alert(`На рахунку залишилося ${a} кредитів`)
+}
+else{
+    alert("На рахунку недостатньо коштів для проведення операції!")
+}
+alert("Операція завершена")  */
 
 
-/* let k1 = "5px"
-let k2 = "12djd334"
-let k3 = "12.45asdwe12"
-let k4 =  "qwqweeewq"
-console.log(Number.parseInt(k1))
-console.log(Number.parseInt(k2))
-console.log(Number.parseInt(k3))
-console.log(Number.parseInt(k4)) */
-
-
-
-/* let k1 = "5px"
-let k2 = "12djd334"
-let k3 = "12.45asdwe12"
-let k4 =  "qwqweeewq"
-console.log(Number.parseFloat(k1))
-console.log(Number.parseFloat(k2))
-console.log(Number.parseFloat(k3))
-console.log(Number.parseFloat(k4)) */
-
-
-/* console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18)) */
-
-
-
-/* console.log(Math.random()*(19-3)+3) */
-
-/* let a = 23
-let b = 10
-let S = a*b
-console.log(S + "cm^2") */
-
-/* let h = 10
-let d = 4
-let П = 3.14
-R = d/2
-const V = П*R**2*h
-console.log(V)  */
-
-/* let a = 3 
-let b = 4
-k = Math.pow((a, 2) + b, 2)
-k = Math.sqrt(k)
-console.log(k) */
-
+/* let payment = prompt("Ціна товару")
+let discount 
+if( payment >= 100 && payment < 1000){
+    discount = 2
+}
+else if(  payment >= 1000 && payment <= 5000){
+    discount = 5
+}
+else if( payment > 5000){
+    discount = 10
+}
+else{
+    discount = 0
+}
+alert(`Оформляемо замовлення на суму ${payment} зі знижкою ${discount}%`)  */
